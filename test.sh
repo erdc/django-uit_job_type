efd57f0a8a987ce3c2024e9e969633ac79da6fa2
Chandler Scott <cscott@aquaveo.com>
2019-03-22 14:58:32 -0600

    Setup CI

--- test.sh
+++ test.sh
@@ -3,12 +3,11 @@
 #    echo "Usage: . test.sh [/path/to/manage.py]";
 #    return 1;
 #fi
+set -e
 rm -f .coverage
 echo "Running Unit Tests..."
 coverage run -a --rcfile=coverage.ini -m unittest -v uit_plus_job.tests.unit_tests.test_oauth2
 
-echo "Running Intermediate Tests..."
-coverage run -a --rcfile=coverage.ini $1 test uit_plus_job.tests.integrated_tests
 echo "Unit Tests Coverage Report..."
 coverage report -m
 echo "Linting..."
